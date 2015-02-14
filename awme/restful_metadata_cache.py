0ba07c6a7c71c0ac25a2ac4040c5cde4d2393b13
Christopher Vincelette <chriss@chrissvincelette.com>
2015-02-13 17:26:11 -0800

    [OPS-1767] not happy with this module yet. I need to re-write it cleaner. But it works.

--- awme/restful_metadata_cache.py
+++ awme/restful_metadata_cache.py
@@ -16,9 +16,24 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-host_metadata_by_instance_id_dict = None
-security_group_info_by_sg_id_dict = None
+if not os.path.isfile('../config/config.ini'):
+    logger.error("Unable to load config.ini file!")
+    exit(1)
+else:
+    logger.debug("Found config.ini file.")
 
+#not supported until python 2.7
+#config = ConfigParser.RawConfigParser(allow_no_value=True)
+config = ConfigParser.RawConfigParser()
+config.read('../config/config.ini')
+
+config_persistence_dir = config.get('awme_general', 'persistence_dir')
+config_supported_regions = config.get('awme_general', 'supported_regions').strip().split(',')
+
+host_metadata_by_region_dict = pickle.load(open("%s/host_metadata.pickle.tmp" % config_persistence_dir, "rb"))
+security_group_metadata_by_region_dict = pickle.load(open("%s/security_group_metadata.pickle.tmp" % config_persistence_dir, "rb"))
+
+logger.debug("Data loaded into memory from: [%s]!" % config_persistence_dir)
 config_supported_regions = list()   
 config_persistence_dir = "/dev/shm"
 
@@ -29,54 +44,36 @@ def index():
 
 @app.route('/ops/api/v1.0/host_instances', methods=['GET'])
 def get_all_host_instances():
-    return jsonify({'host-instances': host_metadata_by_instance_id_dict})
+    return jsonify({'host-instances': host_metadata_by_region_dict})
 
 
 @app.route('/ops/api/v1.0/sg_instances', methods=['GET'])
 def get_all_sg_instances():
-    return jsonify({'security-groups': security_group_info_by_sg_id_dict})
+    return jsonify({'security-groups': security_group_metadata_by_region_dict})
 
 
 @app.route('/ops/api/v1.0/host_instances/<string:instance_id>', methods=['GET'])
 def get_host_instance_by_id(instance_id):
-    if (instance_id not in host_metadata_by_instance_id_dict):
+    if (instance_id not in host_metadata_by_region_dict):
         abort(404)
 
-    return jsonify({'host-instance': host_metadata_by_instance_id_dict[instance_id]})
+    return jsonify({'host-instance': host_metadata_by_region_dict[instance_id]})
 
 
 @app.route('/ops/api/v1.0/sg_instances/<string:sg_id>', methods=['GET'])
 def get_sg_instance_by_id(sg_id):
-    if (sg_id not in security_group_info_by_sg_id_dict):
+    if (sg_id not in security_group_metadata_by_region_dict):
         abort(404)
 
-    return jsonify({'security-group': security_group_info_by_sg_id_dict[sg_id]})
+    return jsonify({'security-group': security_group_metadata_by_region_dict[sg_id]})
 
 @app.route('/ops/api/v1.0/regions', methods=['GET'])
 def get_regions():
     return jsonify({'supported-regions': config_supported_regions})
 
-def main():
+def main():   
+    #launch server
     app.run(host='0.0.0.0', port=10080, debug=True)
-    
-    if not os.path.isfile('../config/config.ini'):
-        logger.error("Unable to load config.ini file!")
-        exit(1)
-    else:
-        print "Found config.ini file."
-    
-    #not supported until python 2.7
-    #config = ConfigParser.RawConfigParser(allow_no_value=True)
-    config = ConfigParser.RawConfigParser()
-    config.read('../config/config.ini')
-    
-    config_persistence_dir = config.get('awme_general', 'persistence_dir')
-    config_supported_regions = config.get('awme_general', 'supported_regions').strip().split(',')
-    
-    host_metadata_by_instance_id_dict = pickle.load(open("%s/host_metadata.pickle.tmp" % config_persistence_dir, "rb"))
-    security_group_info_by_sg_id_dict = pickle.load(open("%s/security_group_metadata.pickle.tmp" % config_persistence_dir, "rb"))
-    
-    logger.debug("Data loaded into memory from: [%s]!" % config_persistence_dir)
-    
+
 if __name__ == '__main__':
     main()
