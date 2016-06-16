1fed8ccf1cb1038593267edae705451505467b75
Christopher Vincelette <cvincelette@roku.com>
2016-06-15 18:49:27 -0700

    Removed some cruft. :)

--- scripts/awme_collector.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import awme.amazon_metadata_collector
-
-def main():
-    awme.amazon_metadata_collector.main()
-
-if __name__ == '__main__':
-    main()
--- scripts/awme_server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import awme.restful_metadata_cache
-
-def main():
-    awme.restful_metadata_cache.main()
-
-if __name__ == '__main__':
-    main()
